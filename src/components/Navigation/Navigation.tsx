a07402f1f19645d01ce49281fa38a816b5f15453
anonymousrecords <a.Records2022@gmail.com>
2024-06-28 17:45:28 +0900

    feat : Navigation 컴포넌트 구현

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,51 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { navList } from "../../constants";
+import { useNavigate } from "react-router-dom";
+
+function Navigation() {
+  const navigate = useNavigate();
+  console.log("params", window.location.pathname);
+  const isActiveNav = (path: string) => {
+    return window.location.pathname === path;
+  };
+  return (
+    <nav css={navigationStyle}>
+      {navList.map((item) => (
+        <button
+          style={{
+            backgroundColor: isActiveNav(item.url) ? "yellow" : "white",
+          }}
+          css={buttonStyle}
+          key={item.id}
+          onClick={() => {
+            navigate(`/${item.url}`);
+          }}
+        >
+          {item.name}
+        </button>
+      ))}
+    </nav>
+  );
+}
+
+export default Navigation;
+
+const navigationStyle = css`
+  display: flex;
+  gap: 10px;
+  margin: 30px 0;
+`;
+
+const buttonStyle = css`
+  width: 100px;
+  height: 30px;
+  border: 2px solid black;
+  border-radius: 3px;
+  color: black;
+  font-weight: bold;
+  cursor: pointer;
+  &:hover {
+    background-color: yellow;
+  }
+`;
---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export const navList = [
+  { id: 0, name: "CSS", url: "/css" },
+  {
+    id: 1,
+    name: "JavaScript",
+    url: "javascript",
+  },
+  {
+    id: 2,
+    name: "React",
+    url: "react",
+  },
+];
