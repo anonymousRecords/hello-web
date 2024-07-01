b281a37b3a517323c08a4d8d32f5c17bd38e6fc8
anonymousrecords <a.Records2022@gmail.com>
2024-07-01 11:39:02 +0900

    feat : Timer 구현중

--- src/ReactPage/ReactArchive/0/React0.tsx
+++ src/ReactPage/ReactArchive/0/React0.tsx
@@ -12,6 +12,7 @@ const BUTTONS = [
   { id: "stopwatch", text: "스톱워치" },
 ];
 
+// 버튼 컴포넌트
 interface ButtonProps {
   id: string;
   text: string;
@@ -20,12 +21,56 @@ interface ButtonProps {
 }
 const Button = ({ id, text, isActive, onClick }: ButtonProps) => (
   <button
-    css={buttonStyle}
+    css={toggleButtonStyle}
     onClick={() => onClick(id)}
-    style={{ backgroundColor: isActive ? "pink" : "#333" }}
+    style={{
+      backgroundColor: isActive ? "pink" : "#333",
+      opacity: isActive ? 1 : 0.1,
+    }}
   >
     {text}
   </button>
+);
+
+// 셀 컴포넌트
+interface CellProps {
+  num: number;
+}
+const Cell = ({ num }: CellProps) => <div css={cellStyle}>{num}</div>;
+
+// 타이머 컴포넌트
+
+const Timer = () => (
+  <div css={timerWrapperStyle}>
+    <div css={cellContainerStyle}>
+      <Cell num={0} />
+      <Cell num={0} />
+      <div>
+        <button>up</button>
+        <button>down</button>
+      </div>
+    </div>
+    <span>:</span>
+    <div css={cellContainerStyle}>
+      <Cell num={0} />
+      <Cell num={0} />
+    </div>
+    <span>:</span>
+    <div css={cellContainerStyle}>
+      <Cell num={0} />
+      <Cell num={0} />
+    </div>
+    <div css={cellContainerStyle}>
+      <button css={timerButtonStyle}>시작</button>
+      <button css={timerButtonStyle}>리셋</button>
+    </div>
+  </div>
+);
+
+const Stopwatch = () => (
+  <div>
+    <h1>스톱워치</h1>
+  </div>
 );
 
 function React0() {
@@ -50,7 +95,7 @@ function React0() {
           ))}
         </nav>
         <section>
-          {activeButton === "timer" ? <div>타이머</div> : <div>스톱워치</div>}
+          {activeButton === "timer" ? <Timer /> : <Stopwatch />}
         </section>
       </main>
     </>
@@ -59,12 +104,51 @@ function React0() {
 
 export default React0;
 
-const buttonStyle = css`
+const toggleButtonStyle = css`
   background-color: #333;
   color: white;
   border: none;
   padding: 10px;
   margin: 10px;
+  cursor: pointer;
+  font-size: 1rem;
+  border-radius: 5px;
+  &:hover {
+    background-color: #555;
+  }
+`;
+
+const cellContainerStyle = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 3px;
+`;
+
+const cellStyle = css`
+  width: 30px;
+  height: 60px;
+  border: 1px solid #333;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const timerWrapperStyle = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
+
+const timerButton
+
+const timerButtonStyle = css`
+  background-color: #333;
+  color: white;
+  border: none;
+  padding: 10px;
   cursor: pointer;
   font-size: 1rem;
   border-radius: 5px;
