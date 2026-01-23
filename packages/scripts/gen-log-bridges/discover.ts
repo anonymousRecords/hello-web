fe8328b179e735b5acebee98fe45021385b95f4b
anonymousRecords <a.Records2022@gmail.com>
2026-01-23 21:55:41 +0900

    refactor: 불필요한 코드 삭제

--- packages/scripts/gen-log-bridges/discover.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export function getLogFolders(rootDir: string): string[] {
-  if (!fs.existsSync(rootDir)) return [];
-  return fs
-    .readdirSync(rootDir)
-    .filter((d) => fs.statSync(path.join(rootDir, d)).isDirectory());
-}
